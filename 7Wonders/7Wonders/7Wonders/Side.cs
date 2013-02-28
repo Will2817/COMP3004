58f0349665b7762dee7b16c7d89a330cbd977336
Will2817 <williamclifford@cmail.carleton.ca>
2013-02-28 12:41:25 -0500

    Started on restructuring wonders
    There is a new object Structure.
    The person creating the Card Object should make sure to include a
    Structure in the Card Object

---
+++ 7Wonders/7Wonders/7Wonders/Side.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Newtonsoft.Json.Linq;
+
+namespace _7Wonders
+{
+    class Side
+    {
+        List<Structure> stages;
+
+
+        public Side(JObject j)
+        {
+
+            stages = new List<Structure>();
+
+            foreach (JObject stage in j["stages"])
+            {
+                stages.Add(new Structure((JArray)stage["cost"], (JArray)stage["effects"]));
+            }
+        }
+    }
+}
---
+++ 7Wonders/7Wonders/7Wonders/Structure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace _7Wonders
+{
+    class Structure
+    {
+        protected List<string> costs;
+        protected List<string> effects;
+
+        public Structure(JArray _costs, JArray _effect)
+        {
+            costs = new List<string>();
+            effects = new List<string>();
+            
+            foreach (JObject j in _costs)
+            {
+                costs.Add((string) j);
+            }
+
+            foreach (JObject j in _effect)
+            {
+                effects.Add((string)j);
+            }
+        }
+
+        public List<string> getEffects()
+        {
+            return effects;
+        }
+
+        public List<string> getCosts()
+        {
+            return costs;
+        }
+    }
+}
