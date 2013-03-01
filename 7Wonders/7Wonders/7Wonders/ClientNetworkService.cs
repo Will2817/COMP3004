b91239167fd49ab7246c8280ecd57b9d7670dd67
Michelle Burrows <mburrows02@gmail.com>
2013-02-28 20:48:22 -0500

    Networking interfaces
    Added some basic networking interfaces.

---
+++ 7Wonders/7Wonders/7Wonders/ClientNetworkService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7Wonders.Networking
+{
+    interface ClientNetworkService
+    {
+        public int joinHost(String hostIP);
+        public int disconnect();
+        public int sendMove(String move);//how to represent move?
+        public int sendMessage(String message);
+    }
+}
---
+++ 7Wonders/7Wonders/7Wonders/HostNetworkService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7Wonders.Networking
+{
+    interface HostNetworkService
+    {
+        public int bootPlayer(String playerIP);//represent player as IP or other?
+        public int blockConnections();
+        public int allowConnections();
+        public int broadcastGameState(String gameState);//broadcast as game state, or action list?
+        public int sendHand(String hand, String playerIP);//hand is a string representation of a JArray; player as IP or other?
+        public int sendWonder(String wonder, String playerIP);//needs to be separate from hand/game state?
+        public int shutdown();
+    }
+}
