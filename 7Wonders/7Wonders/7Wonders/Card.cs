02b266e93b0dd7295c3a7d2b70b41210336f6d30
Danny <hisoka365@gmail.com>
2013-03-02 18:26:09 -0500

    Card Object
    Just the template atm, need to incorperate it with structure etc. Still
    going through your code to familarize myself with it. Also the chat
    added can't compile iwth my vs for some odd reason.

--- 7Wonders/7Wonders/7Wonders/Card.cs
+++ 7Wonders/7Wonders/7Wonders/Card.cs
@@ -13,10 +13,49 @@ using Newtonsoft.Json.Linq;
 
 namespace _7Wonders
 {
-    class Card
+    public class Card
     {
+        protected string name;
+        protected Visual image;
+        protected int players;
+        protected int age;
+        protected string guild;
+        protected Structure cost;
+        protected Structure effect;
 
+        public Card(Game1 theGame, JObject _json)
+        {
+            name = (string)_json["name"];
+            //image = new Visual ??
+            players = (int)_json["players"];
+            age = (int)_json["age"];
+            guild = (string)_json["guild"];
+        }
 
+        public string getName()
+        {
+            return name;
+        }
+
+        public Visual getVisual()
+        {
+            return image;
+        }
+
+        public int getPlayers()
+        {
+            return players;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public string getGuild()
+        {
+            return guild;
+        }
 
         
     }
