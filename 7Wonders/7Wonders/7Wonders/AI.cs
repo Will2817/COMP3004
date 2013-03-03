013d5f694a5bf49830bab9d0276409f665bd06d0
Danny <hisoka365@gmail.com>
2013-03-03 17:46:54 -0500

    Updated AI to match Player via inheritence

--- 7Wonders/7Wonders/7Wonders/AI.cs
+++ 7Wonders/7Wonders/7Wonders/AI.cs
@@ -8,8 +8,8 @@ namespace _7Wonders
     class AI : Player
     {
 
-        public AI(string _name)
-            :base (_name)
+        public AI(string _name, Wonder _board, List<Card> _hand)
+            :base (_name, _board, _hand)
         
         {
         }
