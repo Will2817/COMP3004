a87bbdae8c55eb652bc2fc9243fb44d077540807
Michelle Burrows <mburrows02@gmail.com>
2013-03-03 19:58:41 -0500

    Card colour and resource type enums
    Added enums for card colour and resource type to be used to store those
    values (in card/structure/etc). May add static helper class(es) to parse
    strings into enum values (e.g. Resource resourceFromString(String)).

---
+++ 7Wonders/7Wonders/7Wonders/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7Wonders
+{
+    public enum Resource
+    {
+        WOOD,
+        STONE,
+        ORE,
+        CLAY,
+        GLASS,
+        PAPYRUS,
+        LOOM
+    }
+}
