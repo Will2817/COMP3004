15e91ca011835e6930835456a0ec5b488c402237
Danny <Hisoka365@gmail.com>
2013-03-11 16:58:48 -0400

    Updated Side and Wonderlist.json
    Removed Stage class as it is no longer needed

--- 7Wonders/7Wonders/7Wonders/Side.cs
+++ 7Wonders/7Wonders/7Wonders/Side.cs
@@ -17,18 +17,79 @@ namespace _7Wonders
 
     public class Side
     {
-        int numStages { get; set; }
-        Stage []  stages;
+        // Declaring Variables
+        private List<Dictionary<Resource, int>> stageCosts;
+        private List<List<Effect>> stageEffects;
+
+        private int stages;
 
         public Side(JObject _json)
         {
-            numStages = ((JArray)_json["stages"]).Count();
+            // Initializing Variables
+            stageCosts = new List<Dictionary<Resource, int>>();
+            stageEffects = new List<List<Effect>>();
 
-            /*foreach (JObject stage in (JArray)_json["stages"])
+            stages = 0;
+
+            // foreach loop - going through the JArray of stages
+            // and placing the stageCosts and stageEffects appropriately
+            foreach (JObject stage in (JArray)_json["stages"])
             {
-                stages[]
-            }*/
-            Console.WriteLine("Number of Stages = " + numStages);
+                stageCosts.Add(new Cost((JObject)stage["cost"]).getCost());
+
+                // Creating a temporary list for effects
+                // for that particular stage
+                List<Effect> e = new List<Effect>();    
+                                                        
+                foreach (JObject j in (JArray)stage["effects"])
+                {
+                    Effect effect = new Effect(j);
+                    e.Add(effect);
+                }
+
+                stageEffects.Add(e);
+            }
+
+            stages = stageCosts.Count;
+        }
+
+        // Returns the number of stages in the particular Wonder
+        public int getStages() { return stages; }
+
+        // Returns a Dictionary of Costs for that particular stage
+        public Dictionary<Resource, int> getStageCost(int stageNum)
+        {
+            return stageCosts[stageNum];
+        }
+
+        // Returns a List of Effects for that particular stage
+        public List<Effect> getStageEffects(int stageNum)
+        {
+            return stageEffects[stageNum];
+        }
+
+        // Returns a List of the Dictionary Costs for the Wonder
+        public List<Dictionary<Resource, int>> getAllStageCosts()
+        {
+            return stageCosts;
+        }
+
+        // Returns a List of Effect Lists for the Wonder
+        public List<List<Effect>> getAllStageEffects()
+        {
+            return stageEffects;
+        }
+
+        // Print out the Effects of the Wonder
+        public void printWonderEffects()
+        {
+            Console.WriteLine("Stage#: " + stages);
+            for (int i = 0; i < stageEffects.Count; i++)
+            {
+                Console.WriteLine("\tStage " + (i + 1));
+                for (int j = 0; j < stageEffects[i].Count; j++)
+                    stageEffects[i][j].PrintEffect();
+            }
         }
     }
 }
