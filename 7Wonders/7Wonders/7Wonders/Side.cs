f6768cf70c21e64f87ca27e6fa39dcf0da274c3e
Danny <hisoka365@gmail.com>
2013-03-12 17:35:48 -0400

    syncing

--- 7Wonders/7Wonders/7Wonders/Side.cs
+++ 7Wonders/7Wonders/7Wonders/Side.cs
@@ -13,83 +13,47 @@ using Newtonsoft.Json.Linq;
 
 namespace _7Wonders
 {
-    using Game_Cards;
-
     public class Side
     {
-        // Declaring Variables
-        private List<Dictionary<Resource, int>> stageCosts;
-        private List<List<Effect>> stageEffects;
+        List<Structure> stages;
+        Resource initialResource;
 
-        private int stages;
-
-        public Side(JObject _json)
+        public Side(JObject j)
         {
-            // Initializing Variables
-            stageCosts = new List<Dictionary<Resource, int>>();
-            stageEffects = new List<List<Effect>>();
-
-            stages = 0;
-
-            // foreach loop - going through the JArray of stages
-            // and placing the stageCosts and stageEffects appropriately
-            foreach (JObject stage in (JArray)_json["stages"])
+            switch ((string)j["effects"][0]["type"])
             {
-                stageCosts.Add(new Cost((JObject)stage["cost"]).getCost());
-
-                // Creating a temporary list for effects
-                // for that particular stage
-                List<Effect> e = new List<Effect>();    
-                                                        
-                foreach (JObject j in (JArray)stage["effects"])
-                {
-                    Effect effect = new Effect(j);
-                    e.Add(effect);
-                }
-
-                stageEffects.Add(e);
+                case "c":
+                    initialResource = Resource.CLAY;
+                    break;
+                case "g":
+                    initialResource = Resource.GLASS;
+                    break;
+                case "l":
+                    initialResource = Resource.LOOM;
+                    break;
+                case "o":
+                    initialResource = Resource.ORE;
+                    break;
+                case "p":
+                    initialResource = Resource.PAPYRUS;
+                    break;
+                case "s":
+                    initialResource = Resource.STONE;
+                    break;
+                case "w":
+                    initialResource = Resource.WOOD;
+                    break;
             }
-
-            stages = stageCosts.Count;
-        }
-
-        // Returns the number of stages in the particular Wonder
-        public int getStages() { return stages; }
-
-        // Returns a Dictionary of Costs for that particular stage
-        public Dictionary<Resource, int> getStageCost(int stageNum)
-        {
-            return stageCosts[stageNum];
-        }
-
-        // Returns a List of Effects for that particular stage
-        public List<Effect> getStageEffects(int stageNum)
-        {
-            return stageEffects[stageNum];
-        }
-
-        // Returns a List of the Dictionary Costs for the Wonder
-        public List<Dictionary<Resource, int>> getAllStageCosts()
-        {
-            return stageCosts;
-        }
-
-        // Returns a List of Effect Lists for the Wonder
-        public List<List<Effect>> getAllStageEffects()
-        {
-            return stageEffects;
-        }
-
-        // Print out the Effects of the Wonder
-        public void printWonderEffects()
-        {
-            Console.WriteLine("Stage#: " + stages);
-            for (int i = 0; i < stageEffects.Count; i++)
+            stages = new List<Structure>();
+            foreach (JObject stage in j["stages"])
             {
-                Console.WriteLine("\tStage " + (i + 1));
-                for (int j = 0; j < stageEffects[i].Count; j++)
-                    stageEffects[i][j].PrintEffect();
+                //stages.Add(new Structure((JObject)stage["cost"], (JObject)stage["effects"]));
             }
+        }
+
+        public Resource getIntialResource()
+        {
+            return initialResource;
         }
     }
 }
